bbbd123a263b1b0b3dd4b50b5e782056e230a252
Sergey Desyatkov <desyatkov@gmail.com>
2019-04-09 02:09:14 +0300

    inits

--- convert.py
+++ convert.py
@@ -25,11 +25,17 @@ overall_fields_name = utils.get_overall_names(brands_split, score_field_name=SCO
 result_data = {}
 BRAND_3_TAGS = ('BRAND_1_TAG', 'BRAND_2_TAG', 'BRAND_3_TAG')
 
-# __USER PERMANENT FIELDS
-PERMANENT_FIELDS = ("ResponseId", "Gender", "Age", "FIRST_NAME", "COUNTY", "AGE_TAG", "Regions")
+# __OVERALL 5 values
+def get_overall_id(brand_id=0, overall_id=0):
+    return "Q2_OVERALL_%s_%s" % (brand_id, overall_id)
 
-user_data = pd.read_csv('csvFiles/data-real.csv', delimiter=',')
+# __USER PERMANENT FIELDS
+
+
+PERMANENT_FIELDS = ("ResponseId", "Gender", "Age", "FIRST_NAME", "COUNTY", "AGE_TAG", "Regions")
+user_data = pd.read_csv('csvFiles/data.csv', delimiter=',')
 user_data_dict = user_data.to_dict('records')
+user_data.info(memory_usage='deep')
 
 pp = pprint.PrettyPrinter(indent=4)
 # pp.pprint(brands_filtered)
@@ -37,9 +43,18 @@ pp = pprint.PrettyPrinter(indent=4)
 for user_item in user_data_dict:                # row__review
     for tag in BRAND_3_TAGS:                    # brand
         item = {}
+        overall = {}
         for fields in PERMANENT_FIELDS:         # fields
             item[fields] = user_item[fields]
+            item['overall'] = {}
+
         if not math.isnan(float(user_item[tag])):
+            for inx in range(1, 6):
+                overall_field_name = get_overall_id(int(user_item[tag]), inx)
+                value_list = user_item[overall_field_name].split(' - ')
+                item['overall'][inx] = {'value': value_list[0], 'textual': value_list[1]}
+                print(value_list)
+
             result_data.setdefault(int(user_item[tag]), []).append(item)
 
 # print(tag, user_item[tag])

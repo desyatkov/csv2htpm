c3c01d0f7d40f74d2ac50c8df4de5281457999ab
Sergey Desyatkov <desyatkov@gmail.com>
2019-04-09 18:44:00 +0300

    add rating calc

--- convert.py
+++ convert.py
@@ -5,12 +5,10 @@ import os
 import pandas as pd
 import utils
 import pprint
-import math
 import numpy as np
 
 BRAND_PREFIX_FIELD = 'S5C'
 SCORE_VALUES_NAME = 'Q2_OVERALL_1'
-SCORE_WEIGHTS = [10, 10, 20, 20, 40]
 
 # GET FILE WITH FIELDS DATA(brands, score values)
 df_brands = pd.read_csv('csvFiles/fields-real.csv', delimiter=',', index_col=0, usecols=['Variable Name', 'Label'])
@@ -21,50 +19,25 @@ brands_filtered = utils.dict_filtering(brands_split, filter_val=BRAND_PREFIX_FIE
 overall_fields_name = utils.get_overall_names(brands_split, score_field_name=SCORE_VALUES_NAME)
 
 # GET USER DATA
-
-# __USER RELATES 3 BRANDS
-result_data = {}
-BRAND_3_TAGS = ('BRAND_1_TAG', 'BRAND_2_TAG', 'BRAND_3_TAG')
-
-
-# __OVERALL 5 values
-def get_overall_id(brand_id=0, overall_id=0):
-    return "Q2_OVERALL_%s_%s" % (brand_id, overall_id)
-
-
-# __USER PERMANENT FIELDS
-
-
-PERMANENT_FIELDS = ("ResponseId", "Gender", "Age", "FIRST_NAME", "COUNTY", "AGE_TAG", "Regions")
-user_data = pd.read_csv('csvFiles/data.csv', delimiter=',')
+user_data = pd.read_csv('csvFiles/data-real.csv', delimiter=',')
 user_data_dict = user_data.to_dict('records')
 user_data.info(memory_usage='deep')
 
+SCORE_WEIGHTS = [10, 10, 20, 20, 40]
+BRAND_3_TAGS = ('BRAND_1_TAG', 'BRAND_2_TAG', 'BRAND_3_TAG')
+PERMANENT_FIELDS = ("ResponseId", "Gender", "Age", "FIRST_NAME", "COUNTY", "AGE_TAG", "Regions")
+
+# COLLECTED DATA
+records_collection_list = utils.get_records(user_data_dict, BRAND_3_TAGS, PERMANENT_FIELDS, SCORE_WEIGHTS)
+
 pp = pprint.PrettyPrinter(indent=4)
+# pp.pprint(records_collection_list)
 
-for user_item in user_data_dict:  # row__review
-    for tag in BRAND_3_TAGS:  # brand
-        item = {}
-        overall = {}
-        score_list = [];
-        for fields in PERMANENT_FIELDS:  # fields
-            item[fields] = user_item[fields]
-            item['overall'] = {}
 
-        if not math.isnan(float(user_item[tag])):
-            for inx in range(1, 6):
-                overall_field_name = get_overall_id(int(user_item[tag]), inx)
-                value_list = user_item[overall_field_name].split(' - ')
-                item['overall'][inx] = {'value': value_list[0], 'textual': value_list[1]}
-                score_list.append(int(value_list[0]))
+rating_default = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0}
 
-            item['overall']['overall_weights'] = SCORE_WEIGHTS
-            item['overall']['overall_list'] = score_list
-            item['overall']['overall_average'] = np.average(score_list)
-            item['overall']['overall_average_weight'] = np.average(score_list, weights=SCORE_WEIGHTS)
-            item['overall']['overall_average_score'] = utils.round_of_rating(item['overall']['overall_average'])
+per_product_avg_rating = utils.get_avg_score(records_collection_list)
 
-            result_data.setdefault(int(user_item[tag]), []).append(item)
 
-# print(tag, user_item[tag])
-pp.pprint(result_data)
+pp.pprint(records_collection_list)
+# pp.pprint(per_product_avg_rating)
--- utils.py
+++ utils.py
@@ -1,3 +1,7 @@
+import math
+import numpy as np
+
+
 def dict_filtering(dict_val, filter_val='S5C'):
     filtered_dict = {}
     for key, value in dict_val.items():
@@ -19,3 +23,55 @@ def get_overall_names(dict_val, count=5, score_field_name='Q2_OVERALL_1'):
 def round_of_rating(number):
     return round(number * 2) / 2
 
+
+def get_overall_id(brand_id=0, overall_id=0):
+    return "Q2_OVERALL_%s_%s" % (brand_id, overall_id)
+
+
+def get_records(user_data_dict, three_brands=(), permanent_fields=(), weight=[20, 20, 20, 20, 20]):
+    result_data = {}
+    for user_item in user_data_dict:  # row__review
+        for tag in three_brands:  # brand
+            item = {}
+            score_list = []
+            for fields in permanent_fields:  # fields
+                item[fields] = user_item[fields]
+                item['overall'] = {}
+
+            if not math.isnan(float(user_item[tag])):
+                for inx in range(1, 6):
+                    overall_field_name = get_overall_id(int(user_item[tag]), inx)
+                    value_list = user_item[overall_field_name].split(' - ')
+                    item['overall'][inx] = {'value': value_list[0], 'textual': value_list[1]}
+                    score_list.append(int(value_list[0]))
+
+                item['overall']['overall_weights'] = weight
+                item['overall']['overall_list'] = score_list
+                item['overall']['overall_average'] = np.average(score_list)
+                item['overall']['overall_average_weight'] = np.average(score_list, weights=weight)
+                item['overall']['overall_average_score'] = round_of_rating(item['overall']['overall_average'])
+
+                result_data.setdefault(int(user_item[tag]), []).append(item)
+    return result_data
+
+
+def get_avg_score(records_collection_list):
+    per_product_avg_rating = {}
+    for key, value in records_collection_list.items():
+        per_product_avg_row = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0}
+        list_length = len(value)
+
+        for item in value:
+            for idx in range(1, 6):
+                per_product_avg_row[idx] += int(item['overall'][idx]['value'])
+
+        for x, y in per_product_avg_row.items():
+            per_product_avg_row[x] = round((y / list_length) * 2, 1)
+
+        per_product_avg_rating[key] = per_product_avg_row
+
+        per_product_avg_rating[key]['val_list'] = list(per_product_avg_row.values())
+        per_product_avg_rating[key]['prod_length'] = list_length
+        per_product_avg_rating[key]['val_average'] = round(np.average(per_product_avg_rating[key]['val_list']), 1)
+
+    return per_product_avg_rating
